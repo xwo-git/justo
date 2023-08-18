4e1001d9d42351ced2da2ae6b9a0af4f0216e321
xwo-git.Max-Ange <xwo-git@maxange.deslandes@gmail.com>
2023-08-18 10:55:34 +0200

    Ajout de la page admin et de son menu de naviguation croquis de la page final admin.php realiser

---
+++ admin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style-admin.css">
+  <script src="app.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap" rel="stylesheet"> 
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<script type="text/javascript" src="vanilla-tilt.js"></script>
+<script src="app.js"></script>
+</head>
+<body>
+    <div class="nav">
+        <div class="left-nav-bar-admin lnba">
+            <a href="index.php" class="link-accueil-admin">Accueil</a>
+        </div>
+        <div class="center-nav-bar-admin cnba">
+            <p>ADMIN</p>
+        </div>
+        <div class="right-nav-bar-admin rnba">
+            <a href="#" class="messagerie-link"><ion-icon name="mail-unread-outline" class="messagerie icon"></ion-icon></a>
+        </div>
+    </div>
+    <hr>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
